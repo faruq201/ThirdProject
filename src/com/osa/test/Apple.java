4f518ac7f8cecee9ba400ce9fb73b4aac711d91e
Md faruq Hosain <faruqhosain2008@gmail.com>
2021-10-31 09:44:26 -0400

    Adding my Home work

---
+++ src/com/osa/test/Apple.java
@@ -0,0 +1,25 @@
+package com.osa.test;
+
+public class Apple {
+
+	public static void main(String[] args) {
+	
+	char a='F';
+	char b='H';	
+	System.out.println(a);	
+	System.out.println(b);		
+		
+    boolean faruq=false;
+    System.out.println(faruq);
+	
+	String f="I am from Bangladesh";
+	String h="I am living New York";	
+	System.out.println(f);
+	System.out.println(h);
+	
+	
+	
+	
+	}
+	
+}
---
+++ src/com/osa/test/Calculator.java
@@ -0,0 +1,30 @@
+package com.osa.test;
+
+public class Calculator {
+
+public static void main(String[] args) {
+	
+  int a=5;
+  int b=7;
+  int c=a+b;
+  
+	System.out.println(c);
+	
+	double x=5.567;
+	double y=3479.786;
+	double z=x+y;
+	System.out.println(z);
+	
+	
+	
+	
+}	
+ 
+	
+	
+}
+
+  
+
+	
+
